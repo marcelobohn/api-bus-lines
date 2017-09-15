f2ff40f032e0a975f84cb7db06a69239d97321d2
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-09-15 15:09:18 -0300

    review code

--- test/test-api.js
+++ test/test-api.js
@@ -11,20 +11,18 @@ describe("route", function() {
     it('POST api lines', (done) => {
       chai.request(server)
         .post('/api/lines')
-        .end((err, res) => {
+        .end(async (err, res) => {
           expect(res.status).to.equal(200);
           expect(res.body).to.be.an('object');
           expect(res).to.be.json;
-
-          expect(res.body).to.include.keys(['linesCount', 'lines']);
-          
-          expect(res.body.linesCount).to.equal('1');
-          
-          var firstLine = res.body.lines[0];
+          expect(res.body).to.include.keys(['lines']);
+          expect(res.body.lines).to.have.lengthOf(1);
+          var firstLine = await res.body.lines[0];
+          expect(firstLine).to.include.keys(['originalData','type','departure','arraival','busStops','obs']);
           expect(firstLine.originalData).to.deep.include({"description": "07:10-IV-FDL-NH","group": "IV-NH-SL - SEGUNDA À SEXTA-FEIRA"});
           expect(firstLine.type).to.equal('intercity');
           expect(firstLine.departure).to.deep.include({"city": "Ivoti","hour": "07:10"});
-          expect(firstLine.arraival).to.deep.include({"city": "Novo Hamburgo"});          
+          expect(firstLine.arraival).to.deep.include({"city": "Novo Hamburgo"});
 
           done();
         });
