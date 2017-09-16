2fda7b967aa5692cffe303796d0c446ec998c8b7
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-09-16 16:16:36 -0300

    refactoring lines and routes

--- routes/api.js
+++ routes/api.js
@@ -2,9 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 const lines = require('../data/lines');
+const routes = require('../data/routes');
 
 router.post('/lines', function(req, res, next) {
   res.json(lines);  
+});
+
+router.post('/routes', function(req, res, next) {
+  res.json(routes);  
 });
 
 module.exports = router;
--- test/test-api.js
+++ test/test-api.js
@@ -11,21 +11,41 @@ describe("route", function() {
     it('POST api lines', (done) => {
       chai.request(server)
         .post('/api/lines')
-        .end(async (err, res) => {
+        .end((err, res) => {
           expect(res.status).to.equal(200);
           expect(res.body).to.be.an('object');
           expect(res).to.be.json;
           expect(res.body).to.include.keys(['lines']);
           expect(res.body.lines).to.have.lengthOf(1);
-          var firstLine = await res.body.lines[0];
-          expect(firstLine).to.include.keys(['originalData','type','departure','arraival','busStops','obs']);
+          var firstLine = res.body.lines[0];
+          expect(firstLine).to.include.keys(['originalData','route','departureHour','obs']);
           expect(firstLine.originalData).to.deep.include({"description": "07:10-IV-FDL-NH","group": "IV-NH-SL - SEGUNDA À SEXTA-FEIRA"});
-          expect(firstLine.type).to.equal('intercity');
-          expect(firstLine.departure).to.deep.include({"city": "Ivoti","hour": "07:10"});
-          expect(firstLine.arraival).to.deep.include({"city": "Novo Hamburgo"});
+          expect(firstLine.route).to.equal('IV-FDL-NH');
+          expect(firstLine.departureHour).to.deep.include('07:10');
 
           done();
         });
     });    
+
+    it('POST api routes', (done) => {
+      chai.request(server)
+        .post('/api/routes')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res).to.be.json;
+          expect(res.body).to.include.keys(['routes']);
+          expect(res.body.routes).to.have.lengthOf(1);          
+          var firstRoute = res.body.routes[0];
+          expect(firstRoute).to.include.keys(['resume','type','departure','arraival','obs']);
+          expect(firstRoute.resume).to.equal('IV-FDL-NH');
+          expect(firstRoute.type).to.equal('intercity');
+          expect(firstRoute.departure).to.deep.include({"city": "Ivoti"});
+          expect(firstRoute.arraival).to.deep.include({"city": "Novo Hamburgo"});
+          expect(firstRoute.obs).to.equal('Via federal');
+
+          done();
+        });
+    });        
   });
 });
