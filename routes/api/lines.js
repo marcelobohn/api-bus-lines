cfcaf3813e95a6624b217c6510112a6e136799c5
Marcelo Bohn <marcelo.bohn@gmail.com>
2017-09-27 10:47:39 -0300

    refactor variables

--- routes/api/lines.js
+++ routes/api/lines.js
@@ -2,22 +2,22 @@ var express = require('express');
 var router = express.Router();
 var decache = require('decache');
 
-const lines = require('../../models/lines');
+const modelLines = require('../../models/lines');
 
 router.post('/:enterprise', function(req, res, next) {
   const { enterprise, route } = req.params;
   const { filterRoute } = req.body;
-  let r = lines.read().get(enterprise).value();
+  let lines = modelLines.read().get(enterprise).value();
   if (filterRoute)
-    r.items = r.items.filter((l) => l.route === filterRoute);
-  res.json(r);
+    lines.items = lines.items.filter((l) => l.route === filterRoute);
+  res.json(lines);
 });
 
 router.post('/:enterprise/:id', function (req, res) {
   const { enterprise, id } = req.params;
-  const r = lines.read().get(enterprise).get('items').value().find(a => a.id === id);
-  if (r)
-    res.json(r);
+  const lines = modelLines.read().get(enterprise).get('items').value().find(a => a.id === id);
+  if (lines)
+    res.json(lines);
   else
     res.status(404).json({"error": "Not found line"});
 });
--- routes/api/routes.js
+++ routes/api/routes.js
@@ -1,22 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-const routes = require('../../models/routes');
+const modelRoutes = require('../../models/routes');
 
 router.post('/:enterprise', function(req, res, next) {
   const { enterprise } = req.params;
   const { filterDepartureCity } = req.body;
-  let r = routes.read().get(enterprise).value();  
+  let routes = modelRoutes.read().get(enterprise).value();  
   if (filterDepartureCity)
-    r.items = r.items.filter((l) => l.departure.city === filterDepartureCity);  
-  res.json(r);  
+    routes.items = routes.items.filter((l) => l.departure.city === filterDepartureCity);  
+  res.json(routes);  
 });
 
 router.post('/:enterprise/:resume', function (req, res) {
   const { enterprise, resume } = req.params;
-  const r = routes.read().get(enterprise).get('items').value().find(a => a.resume === resume);
-  if (r)
-    res.json(r);
+  const routes = modelRoutes.read().get(enterprise).get('items').value().find(a => a.resume === resume);
+  if (routes)
+    res.json(routes);
   else
     res.status(404).json({"error": "Not found route"});
 });
